fab407c56d581a92a07c37da54a173c501b8cfdc
zcl <pianozcl@gmail.com>
2020-09-28 21:27:20 +0800

    1.添加基础数据结构 2.求二叉树头结点到叶子结点到所有路径和 3.反转链表 4.反转数字，并考虑溢出情况

---
+++ src/basestructure/TreeNode.java
@@ -0,0 +1,16 @@
+package basestructure;
+
+/**
+ * @author : chenliangzhou
+ * create at:  2020/9/28  2:10 PM
+ * @description: 二叉树
+ **/
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
