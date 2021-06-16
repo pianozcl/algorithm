7b5647c65529c7b26486e58d450e7ae92ad417e8
pianozcl <pianozcl@gmail.com>
2021-06-16 23:42:14 +0800

    practice mapreduce

--- src/main/java/other/bigdata/hadoop/WordCount.java
+++
@@ -1,9 +0,0 @@
-package other.bigdata.hadoop;
-
-/**
- * @author : chenliangzhou
- * create at:  2021/5/21  2:47 PM
- * @description:
- **/
-public class WordCount {
-}
---
+++ src/main/java/other/bigdata/hadoop/mapreduce/wordcount/WordCountMapper.java
@@ -0,0 +1,36 @@
+package other.bigdata.hadoop.mapreduce.wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+/**
+ * @author : chenliangzhou
+ * create at:  2021/6/16  10:31 PM
+ * @description:
+ **/
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private Text outK = new Text();
+    private IntWritable outV = new IntWritable(1);
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        //1 获取一行
+        String line = value.toString();
+
+        //切割
+        String[] words = line.split(" ");
+
+
+        for (String word : words) {
+
+            outK.set(word);
+
+            //写出
+            context.write(outK, outV);
+        }
+    }
+}
